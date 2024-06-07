8999178fdc1f83ddd49d6b9e59628e1de4d2f704
joel <joelnunes974@gmail.com>
2024-06-07 16:45:34 -0300

    login screen - save jwt on local storage

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 import './login.css'
-import Person from '../../img/person.png'
+import Logo from '../../img/logo.png'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 function Login() {
   
-  let navigate = useNavigate(); 
+
+const [values, setValues] = useState(
+  {userEmail:"", passwd:""}
+);
+
+const navigate = useNavigate()
+
+const handleSubmit = async (e)=> {
+  e.preventDefault()
+  try {
+   const response = await axios.post(`http://${process.env.REACT_APP_URL}:${process.env.REACT_APP_PORT}/user/login`, values)
+   if(response.data.auth = true){
+    navigate('/')
+   }
+   else{
+    alert("WRONG EMAIL OR PASSWORD")
+   }
+  
+  } catch (error) {
+    console.log(error)  
+  }
+}
 
   
   return (
@@ -14,33 +38,40 @@ function Login() {
       <div className='main-container'>
         
         <div className='left-box'>
-          <img src={Person} className='image'/> 
+      
         </div>
+
         <div className='login-box'>
-          <div className="title-container"> 
-            <h1>Welcome</h1>
-            <h2>Sign in to your account</h2>
+          <div className='logo-container'>
+            <h1>Reader Rating</h1>
+            <img src={Logo} className='logo'/>
+          </div>
+          <div className="title-container">  
+            <h2>Welcome</h2>
+            <h3>Sign in to your account</h3>
           </div>
           
           <div className='container-form'>
-            <Form>
+            <Form type="form" className='form' onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Control type="email" placeholder="Enter email" className="formControl" value={values.userEmail} onChange={(e) => setValues({...values,userEmail: e.target.value})} />
                   <Form.Text className="text-muted">
                   We'll never share your email with anyone else.
                   </Form.Text>
               </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
+                  <Form.Control type="password" placeholder="Password" className="formControl"  value={values.passwd} onChange={(e) => setValues({...values,passwd: e.target.value})} />
                 </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Check me out" />
               </Form.Group>
-              <Button variant="primary" type="submit">
-              Submit
+              <div className='button-container'>
+              <Button variant="outline-secondary" type="submit" className='buttonSubmit'>
+                <img src="https://cdn-icons-png.flaticon.com/512/154/154346.png"/> 
               </Button>
+              </div>
             </Form>
           </div>
         </div>
